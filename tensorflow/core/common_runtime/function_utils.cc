b9f49fe4682895df025a75656b14569e01bf0346
gaoyong10 <gaoyong10@huawei.com>
2022-04-28 20:30:32 +0800

    Modified   mindspore/ccsrc/utils/anfalgo.cc

--- tensorflow/core/common_runtime/function_utils.cc
+++ tensorflow/core/common_runtime/function_utils.cc
@@ -75,11 +75,11 @@ static Node* AddIdentity(StringPiece name, Graph* g, Endpoint input) {
 
 void DumpGraph(StringPiece label, const Graph* g) {
   // TODO(zhifengc): Change Graph to record #nodes.
-  VLOG(2) << "Graph " << label << " #nodes " << g->num_nodes() << " #edges "
+  LOG(ERROR) << "Graph " << label << " #nodes " << g->num_nodes() << " #edges "
           << g->num_edges();
-  if (VLOG_IS_ON(5)) {
+  if (true) {
     for (const auto& line : str_util::Split(DebugString(g), '\n')) {
-      VLOG(5) << "|| " << line;
+      LOG(ERROR) << "|| " << line;
     }
   }
 }
